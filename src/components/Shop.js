0adccadacbd152f9ecf325066d80f0794eea7391
Akshaj2506 <74644487+Akshaj2506@users.noreply.github.com>
2024-01-08 12:08:03 +0530

    installed redux and set up action creators and amount reducers

--- src/components/Shop.js
+++ src/components/Shop.js
@@ -3,11 +3,10 @@ import React from 'react'
 export const Shop = () => {
   return (
     <div>
-      <h1>Apple Mac Book Pro M2</h1>
+      <h1>Deposit/Withdraw Money</h1>
       <button className="btn btn-danger me-2">-</button>
-      <span>Add to cart</span>
+      <span>Update Balance</span>
       <button className="btn btn-success mx-2">+</button>
-
     </div>
   )
 }
