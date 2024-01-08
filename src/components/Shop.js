caf752666a918f811f2e6dba5eaf88cadd2b0733
Akshaj2506 <74644487+Akshaj2506@users.noreply.github.com>
2024-01-08 11:29:45 +0530

    created the basic app structure for redux app

---
+++ src/components/Shop.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+export const Shop = () => {
+  return (
+    <div>
+      <h1>Apple Mac Book Pro M2</h1>
+      <button className="btn btn-danger me-2">-</button>
+      <span>Add to cart</span>
+      <button className="btn btn-success mx-2">+</button>
+
+    </div>
+  )
+}
